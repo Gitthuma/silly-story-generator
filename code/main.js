facaabc03a5d1a70b2b9982a34d64d2e0e99da0d
Gitthuma <george.githuma@gmail.com>
2021-04-13 02:45:01 +0300

    Created variables for text strings to be used as inputs for the program

--- code/main.js
+++ code/main.js
@@ -1,4 +1,4 @@
-/*1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS*/
+/*COMPLETE VARIABLE AND FUNCTION DEFINITIONS*/
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -8,3 +8,14 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+/*Create variables for text strings to be used as inputs for the program*/
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+
+let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
