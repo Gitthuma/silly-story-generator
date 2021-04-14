7008f3ef7c30b19ee8f394e3a1fe2f6258d35915
Gitthuma <george.githuma@gmail.com>
2021-04-14 23:56:54 +0300

    Replaced story text string with the proper text

--- code/main.js
+++ code/main.js
@@ -11,7 +11,7 @@ function randomValueFromArray(array){
 
 /*Create variables for text strings to be used as inputs for the program*/
 
-let storyText = "It was 94 fahrenheit outside, so  went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
